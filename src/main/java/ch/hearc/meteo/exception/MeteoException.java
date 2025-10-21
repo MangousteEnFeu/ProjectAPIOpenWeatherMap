d851bedb36e34e32258e764bc3aa044317ed118f
vuilthe <theo.vuillaume@he-arc.ch>
2025-10-21 17:21:58 +0200

    jout de la classe placeholder MeteoException pour futures erreurs métier

---
+++ src/main/java/ch/hearc/meteo/exception/MeteoException.java
@@ -0,0 +1,9 @@
+package ch.hearc.meteo.exception;
+
+/**
+ * Classe d’exception générique prévue pour les erreurs liées à la logique métier météo.
+ * Actuellement vide : à compléter ou supprimer si elle n’est pas utilisée.
+ * Idée : pourrait hériter de RuntimeException pour uniformiser la gestion d’erreurs applicatives.
+ */
+public class MeteoException {
+}
